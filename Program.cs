8997506442776ed2b953c79a16285288c6dfe01a
Mohammad <Mohammad@DESKTOP-JI6JPR0>
2023-03-06 21:33:38 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("please enter score");
+int x;
+x = 0;
+x = Convert.ToInt32(Console.ReadLine());
+
+switch (x)
+{
+    case <= 20 and >= 17:
+
+        Console.WriteLine(" your score is A");
+        break;
+    case < 17 and >= 15:
+        Console.WriteLine(" your score is B");
+        break;
+        case <15 and >= 10:
+        Console.WriteLine("your score is C");
+        break;
+    case < 10 and <= 0:
+        Console.WriteLine("your score is F");
+        break ;
+        default: 
+        Console.WriteLine("please enter a number between 0 and 20");
+        break;
+}
